d49205da37eb5ef192ba8c8c76ba9928349dd5ef
amanali1896 <pamanalionline@gmail.com>
2019-11-06 17:47:54 -0500

    Update strings.py

---
+++ strings.py
@@ -0,0 +1,7 @@
+# 'hello' a string
+# single quotes can include double single quotes. and vice versa
+# a = '1' variable with value '1'.
+# 'a' + 'b' = ab
+# [] can be used to get a substring
+# [1,3] you get second and fourth letter from substring
+#
